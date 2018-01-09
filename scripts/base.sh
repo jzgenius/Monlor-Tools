73fe69acd8245bbfbd28b589209058e0c1336da6
monlor <monlor@qq.com>
2018-01-09 23:33:10 +0800

    2018-01-09

---
+++ scripts/base.sh
@@ -0,0 +1,74 @@
+#!/bin/sh
+#copyright by monlor
+
+monlorurl="https://coding.net/u/monlor/p/Monlor-Tools/git/raw/master"
+#monlorurl="https://coding.net/u/monlor/p/Monlor-Test/git/raw/master"
+#monlorurl="https://raw.githubusercontent.com/monlor/Monlor-Tools/master"
+monlorpath=$(uci -q get monlor.tools.path)
+userdisk=$(uci -q get monlor.tools.userdisk)
+monlorconf="$monlorpath/scripts/monlor"
+monlorbackup="/etc/monlorbackup"
+
+result=$(cat /proc/xiaoqiang/model)
+if [ "$result" == "R1D" -o "$result" == "R2D" -o "$result" == "R3D"  ]; then
+	model=arm
+elif [ "$result" == "R3" -o "$result" == "R3P" -o "$result" == "R3G" -o "$result" == "R1CM" ]; then
+	model=mips
+fi
+
+checkuci() {
+	result=$(uci -q get monlor.$1)
+	if [ ! -z "$result" -a -d $monlorpath/apps/$1 ]; then
+		echo 0
+	else
+		echo 1
+	fi
+}
+
+checkread() {
+	if [ "$1" == '1' -o "$1" == '0' ]; then
+		echo -n '0'
+	else
+		echo -n '1'
+	fi
+}
+
+cutsh() {
+
+	test1=$1
+	test2=$2
+	[ -z "$test2" ] && test2=$test1
+	echo `echo $test1 | cut -d, -f$test2`
+
+}
+
+logsh() {
+	
+	logger -s -p 1 -t "$1" "$2"
+	
+}
+
+monitor() {
+	appname=$1
+	[ `checkuci $appname` -eq 1 ] && return
+	service=`uci -q get monlor.$1.service`
+	if [ -z $appname ] || [ -z $service ]; then
+		logsh "【Tools】" "uci配置出现问题！"
+		return
+	fi
+	App_enable=$(uci -q get monlor.$appname.enable) 
+	result=$($monlorpath/apps/$appname/script/$appname.sh status | tail -1) 
+	process=$(ps | grep {$appname} | grep -v grep | wc -l)
+	
+	#检查插件运行异常情况
+	if [ "$process" == '0' ]; then
+		if [ "$App_enable" == '1' -a "$result" == '0' ]; then
+			logsh "【$service】" "$appname运行异常，正在重启..." 
+			$monlorpath/apps/$appname/script/$appname.sh restart 
+		elif [ "$App_enable" == '0' -a "$result" == '1' ]; then
+			logsh "【$service】" "$appname配置已修改，正在停止$appname服务..."   
+			$monlorpath/apps/$appname/script/$appname.sh stop
+		fi
+	fi
+
+}
---
+++ scripts/crontab.sh
@@ -0,0 +1,26 @@
+#!/bin/ash
+#copyright by monlor
+monlorpath=$(uci -q get monlor.tools.path)
+[ $? -eq 0 ] && source "$monlorpath"/scripts/base.sh || exit
+
+logsh "【Tools】" "定时任务crontab.sh启动..."
+# logsh "【Tools】" "获取更新插件列表"
+rm -rf /tmp/applist.txt
+[ "$model" == 'arm' ] && applist="applist.txt"
+[ "$model" == 'mips' ] && applist="applist_mips.txt"
+curl -skLo /tmp/applist.txt $monlorurl/config/"$applist"
+if [ $? -eq 0 ]; then
+	rm -rf $monlorpath/config/applist*.txt
+	mv /tmp/applist.txt $monlorpath/config
+else 
+	logsh "【Tools】" "获取失败，检查网络问题！"
+fi
+
+# logsh "【Tools】" "获取插件版本信息"
+rm -rf /tmp/tools_version.txt
+curl -skLo /tmp/tools_version.txt $monlorurl/config/version.txt 
+mkdir -p /tmp/version > /dev/null 2>&1
+cat $monlorpath/config/applist.txt | while read line
+do
+	[ ! -z $line ] && curl -skLo /tmp/version/$line.txt $monlorurl/apps/$line/config/version.txt 
+done
