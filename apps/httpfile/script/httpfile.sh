2096edb9eb1e17d4b5e7bb8468fc8b1c0490132f
monlor <monlor@qq.com>
2018-01-10 11:48:56 +0800

    2018-01-10

--- apps/httpfile/script/httpfile.sh
+++ apps/httpfile/script/httpfile.sh
@@ -8,34 +8,25 @@ SERVICE_USE_PID=1
 SERVICE_WRITE_PID=1
 SERVICE_DAEMONIZE=1
 
-service=KodExplorer
-appname=kodexplorer
+service=HttpFile
+appname=httpfile
 EXTRA_COMMANDS=" status backup recover"
 EXTRA_HELP="        status  Get $appname status"
-# port=81
-PHPBIN=/opt/bin/spawn-fcgi
-NGINXBIN=/opt/sbin/nginx
-NGINXCONF=/opt/etc/nginx/nginx.conf
-PHPCONF=/opt/etc/php.ini
-WWW=/opt/share/nginx/html
-CONF="/opt/etc/nginx/vhost/kodexplorer.conf"
+port=1688
+BIN=/opt/sbin/nginx
+CONF=$monlorpath/apps/$appname/config/$appname.conf
 LOG=/var/log/$appname.log
-path=$(uci -q get monlor.$appname.path) 
-port=$(uci -q get monlor.$appname.port) || port=81
-opkg_list="php7-cgi php7-mod-curl php7-mod-gd php7-mod-iconv php7-mod-json php7-mod-mbstring php7-mod-opcache php7-mod-session php7-mod-zip nginx spawn-fcgi zoneinfo-core zoneinfo-asia"
+path=$(uci -q get monlor.$appname.path) || path="$userdisk"
+port=$(uci -q get monlor.$appname.port) || port=88
 
 set_config() {
 
-	for i in $opkg_list 
-	do
-		result=$(opkg list-installed | grep -c "^$i")
-		[ "$result" == '0' ] && opkg install $i
-	done
-	
+	result=$(opkg list-installed | grep -c "^nginx")
+	[ "$result" == '0' ] && opkg install nginx
 	if [ ! -f "$NGINXCONF".bak ]; then
 	 	logsh "【$service】" "检测到nginx未配置, 正在配置..."
 		#修改nginx配置文件
-		[ ! -x "$NGINXBIN" ] && logsh "【$service】" "nginx未安装" && exit
+		[ ! -x "$BIN" ] && logsh "【$service】" "nginx未安装" && exit
 		[ ! -f "$NGINXCONF" ] && logsh "【$service】" "未找到nginx配置文件！" && exit
 		cp $NGINXCONF $NGINXCONF.bak
 		cat > "$NGINXCONF" <<-\EOF
@@ -87,71 +78,31 @@ set_config() {
 		}
 		EOF
 	fi
-	#生成配置文件
+	# 生成配置文件
 	[ ! -d "/opt/etc/nginx/vhost" ] && mkdir -p /opt/etc/nginx/vhost
 	rm -rf $CONF
 	cat > "$CONF" <<-\EOF
 	server {
-	        listen  81;
-	        server_name  kodexplorer;
-
+	        listen  88;
+	        server_name  httpfile;
+	        charset utf-8;
 	        location / {
-	            root   /opt/share/nginx/html;
+	            root   directory;
 	            index  index.php index.html index.htm;
-	        }
-
-	        error_page   500 502 503 504  /50x.html;
-	        location = /50x.html {
-	            root   html;
-	        }
-
-	        location ~ \.php$ {
-	            root           /opt/share/nginx/html;
-	            fastcgi_pass   127.0.0.1:9009;
-	            fastcgi_index  index.php;
-	            fastcgi_param  SCRIPT_FILENAME  $document_root$fastcgi_script_name;
-	            include        fastcgi_params;
-	        }
+	            autoindex on;
+	            autoindex_exact_size on;
+	            autoindex_localtime on;
+	        }	     
 	}
 	EOF
-	sed -i "s/81/$port/" $CONF
-	
+	sed -i "s/88/$port/" $CONF
+	sed -i "s/directory/$path/" $CONF
 
-	if [ ! -f "$PHPCONF".bak ]; then
-		logsh "【$service】" "检测到php未配置, 正在配置..."
-		result=$(opkg list-installed | grep -c "^php7-cgi")
-		[ "$result" == '0' ] && logsh "【$service】" "php未安装" && exit
-		[ ! -f "$PHPCONF" ] && logsh "【$service】" "未找到php配置文件！" && exit
-		cp $PHPCONF $PHPCONF.bak
-		sed -i "/doc_root/d" $PHPCONF
-		sed -i "s#.*open_basedir.*#open_basedir = \"$WWW\"#" $PHPCONF
-		sed -i 's/memory_limit = 8M/memory_limit = 20M/' $PHPCONF
-		sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 2000M/' $PHPCONF
-
-		echo "<?php phpinfo(); ?>" > $WWW/info.php
-		rm -rf $WWW/index.html
-	fi
-	if [ ! -d $WWW/app/kod/ ]; then
-		logsh "【$service】" "未检测到$appname文件，正在下载"
-		curl -skLo $WWW/kodexplorer.tar.gz $monlorurl/temp/kodexplorer.tar.gz
-		[ $? -ne 0 ] && logsh "【$service】" "$appname文件下载失败" && exit
-		tar zxvf $WWW/kodexplorer.tar.gz -C $WWW
-		rm -rf $WWW/kodexplorer.tar.gz
-	fi
-	
-	logsh "【$service】" "挂载$appname管理目录"
-	if [ ! -z "$path" ]; then
-		if [ -d $WWW/data/User/admin/home ]; then
-			mount -o blind $path $WWW/data/User/admin/home
-		else
-			logsh "【$service】" "检测到$appname服务未配置，无法挂载管理目录"
-		fi
-	fi
 }
 
 start () {
 
-	result=$(ps | grep -E 'nginx|php-cgi' | grep -v sysa | grep -v grep | wc -l)
+	result=$(ps | grep nginx |  grep -v sysa | grep -v grep | wc -l)
     	if [ "$result" != '0' ];then
 		logsh "【$service】" "$appname已经在运行！"
 		exit 1
@@ -162,7 +113,7 @@ start () {
 		exit 1
 	fi
 	logsh "【$service】" "正在启动$appname服务... "
-	#检查entware状态
+	# 检查entware
 	result1=$(uci -q get monlor.entware)
 	result2=$(ls /opt | grep etc)
 	if [ -z "$result1" ] || [ -z "$result2" ]; then 
@@ -176,28 +127,21 @@ start () {
 	set_config
 	
 	iptables -I INPUT -p tcp --dport $port -m comment --comment "monlor-$appname" -j ACCEPT 
-	/opt/etc/init.d/S80nginx start >> /tmp/messages 2>&1
+	[ ! -f "/opt/etc/init.d/S80nginx" ] && logsh "【$service】" "未找到启动脚本！" && exit
+	/opt/etc/init.d/S80nginx start
 	if [ $? -ne 0 ]; then
-		logsh "【$service】" "启动nginx服务失败！"
-		exit
-	fi
-	$PHPBIN -a 127.0.0.1 -p 9009 -C 2 -f /opt/bin/php-cgi >> /tmp/messages 2>&1   
-	if [ $? -ne 0 ]; then
-                logsh "【$service】" "启动php服务失败！"
+        logsh "【$service】" "启动$appname服务失败！"
 		exit
     fi
-    logsh "【$service】" "$appname服务启动完成"
-    logsh "【$service】" "请在浏览器中访问[http://192.168.31.1:$port]配置"
+    logsh "【$service】" "启动$appname服务完成！"
 
 }
 
 stop () {
 
 	logsh "【$service】" "正在停止$appname服务... "
-	/opt/etc/init.d/S80nginx stop >> /tmp/messages 2>&1
-	killall php-cgi >> /tmp/messages 2>&1
-	kill -9 $(ps | grep -E 'nginx|php-cgi' | grep -v sysa | grep -v grep | awk '{print$1}') > /dev/null 2>&1
-	umount $WWW/data/User/admin/home > /dev/null 2>&1
+	/opt/etc/init.d/S80nginx stop > /dev/null 2>&1
+	ps | grep $BIN | grep -v grep | awk '{print$1}' | xargs kill -9 > /dev/null 2>&1
 	iptables -D INPUT -p tcp --dport $port -m comment --comment "monlor-$appname" -j ACCEPT > /dev/null 2>&1
 
 }
@@ -212,18 +156,19 @@ restart () {
 
 status() {
 
-	result=$(ps | grep -E 'nginx|php-cgi' | grep -v sysa | grep -v grep | wc -l)
-	if [ "$result" -lt '5' ]; then
+	result=$(ps | grep nginx | grep -v sysa | grep -v grep | wc -l)
+	if [ "$result" == '0' ]; then
 		echo "未运行"
 		echo "0"
 	else
-		echo "运行端口号: $port, 管理目录: $path"
+		echo "运行端口号: $port"
 		echo "1"
 	fi
 
 }
 
 backup() {
+	
 	mkdir -p $monlorbackup/$appname
 	echo -n
 }
